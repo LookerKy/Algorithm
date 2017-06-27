abadaca7b485914fcfee2949a06faa71dc5aa767
KyuYoung <nagaraz07@gmail.com>
2017-06-27 23:15:49 +0900

    last test

--- src/com/kyhome/failproblem/problem2251.java
+++ src/com/kyhome/failproblem/problem2251.java
@@ -3,7 +3,7 @@ package com.kyhome.failproblem;
 public class problem2251 {
 	public static void main(String[] args) {
 		System.out.println("fail problem");
-		System.out.println("아 씨발 존나안풀리네");
 		System.out.println("집가서 풀어야지");
+		System.out.println("후 이제 끝난거같다");
 	}
 }
