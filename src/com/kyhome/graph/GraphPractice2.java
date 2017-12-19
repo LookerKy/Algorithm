6ac58323743aa41b471ba33e653fc5c62b6ee550
LookerKy <nagaraz07@gmail.com>
2017-12-19 10:12:52 +0900

    가중치 인접리스트 구현

--- src/com/kyhome/graph/GraphPractice2.java
+++ src/com/kyhome/graph/GraphPractice2.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 /*인접리스트*/
 public class GraphPractice2 {
@@ -11,9 +13,10 @@ public class GraphPractice2 {
 	public static void main(String[] args) throws Exception {
 
 		adjacencyList();
-
+		addAdjacencyList();
 	}
-
+	
+	/*가중치가 없는 인접리스트*/
 	public static void adjacencyList() throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String graph[] = bf.readLine().split(" ");
@@ -44,6 +47,35 @@ public class GraphPractice2 {
 		}
 		System.out.println(sb.toString());
 	}
+	/*무방향 가중치 인접리스트*/
+	public static void addAdjacencyList() throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		ArrayList<TreeMap<Integer, Integer>> list = new ArrayList<TreeMap<Integer,Integer>>();
+		for(int i=0; i<=n; i++) {
+			TreeMap<Integer, Integer> input = new TreeMap<Integer,Integer>();
+			list.add(input);
+		}
+		for(int i=1; i<=m; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			list.get(u).put(v,a);
+			list.get(v).put(u,a);
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=n; i++){
+			sb.append("#"+i+" ");
+			for(int key : list.get(i).keySet()) {
+				sb.append("("+key+","+list.get(i).get(key)+") ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 	
 	
 	/*참고! 간선 리스트: Collection을 사용할 수 없는 경우 
@@ -61,13 +93,13 @@ public class GraphPractice2 {
 4 3
 4 6
 
-6 7
-1 2
-1 5
-2 3
-2 5
-5 4
-4 3
-4 6
+6 7 
+1 2 1
+1 5 3
+2 3 8
+2 5 7
+5 4 12
+4 3 15
+4 6 9
 	  */
 }
