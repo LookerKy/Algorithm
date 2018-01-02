67b2495055e5b2f5ec4f1bce16f2b0253194ee7d
LookerKy <nagarz07@gamil.com>
2018-01-02 21:21:05 +0900

    solved 경로찾기

--- src/com/kyhome/graph/Problem11403.java
+++ src/com/kyhome/graph/Problem11403.java
@@ -3,47 +3,68 @@ package com.kyhome.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Problem11403 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(bf.readLine());
-		int g[][] = new int[n+1][n+1];
-		int result[][] = new int[n+1][n+1];
-		for(int i=1; i<=n; i++) {
+		int g[][] = new int[n + 1][n + 1];
+		int result[][] = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
-			for(int j=1; j<=n; j++) {
+			for (int j = 1; j <= n; j++) {
 				g[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		boolean check[] = new boolean[n+1];
-		for(int i=1; i<=n ; i++) {
-			for(int j=1; j<=n; j++) {
-				dfs(n,g,check,i,j,result);
-			}
-			Arrays.fill(check, false);
+		ArrayList<List<Integer>> list = new ArrayList<List<Integer>>();
+		for (int i = 0; i <= n; i++) {
+			list.add(new ArrayList<Integer>());
 		}
-		for(int i=1;i<=n; i++) {
-			for(int j=1; j<=n; j++) {
-				System.out.print(result[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void dfs(int n,int g[][], boolean check[], int start, int end, int result[][]) {
-		check[start] = true;
-		for(int i=1; i<=n;i++) {
-			if(g[start][i] ==1) {
-				if( i != end && check[i]==false)
-					dfs(n,g,check,i,end,result);
-				else{
-					result[start][end] = 1;
-					return;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (g[i][j] == 1) {
+					list.get(i).add(j);
 				}
 			}
+//			System.out.println(list.get(i).iterator().toString());
+		}
+//		 for(int i=1; i<=n; i++) {
+//		 for(int j=0;j<list.get(i).size(); j++) {
+//		 System.out.print(list.get(i).get(j)+ " ");
+//		 }System.out.println();
+//		 }
+		for (int i = 1; i <= n; i++) {
+			dfs(list, result, i, i);
+		}
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				sb.append(result[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static void dfs(ArrayList<List<Integer>> list, int result[][], int start, int next) {
+		for (int j = 0; j < list.get(next).size(); j++) {
+			int next_t = list.get(next).get(j);
+			if (result[start][next_t] != 1) {
+				result[start][next_t] = 1;
+				dfs(list, result, start, next_t);
+				
+			}
 		}
 	}
+/*틀린경우의수 
+4
+0 0 0 0
+1 0 1 0
+0 1 0 1
+1 0 1 0
+*/
 }
