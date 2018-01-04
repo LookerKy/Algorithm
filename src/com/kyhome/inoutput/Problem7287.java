b35e9142426e95426e25374952fb9cd0abfad5eb
LookerKy <nagarz07@gamil.com>
2018-01-04 18:24:30 +0900

    gjgjgj

---
+++ src/com/kyhome/inoutput/Problem7287.java
@@ -0,0 +1,9 @@
+package com.kyhome.inoutput;
+
+public class Problem7287 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("|\\_/|"+"\n"+"|q p|   /}"+"\n"+"( 0 )\"\"\"\\"+"\n"+"|\"^\"`    |"+"\n"+"||_/=\\\\__|");
+		System.out.println(sb.toString());
+	}
+}
